23011ece0c765394122a28293e83e4d85e789891
TommyPlayer-c <793025902@qq.com>
2021-11-03 16:53:26 +0800

    增加了一个表的外键

--- app.py
+++ app.py
@@ -30,16 +30,17 @@ def forge():
     """Generate fake data."""
     db.create_all()
 
+    singerDF = pd.read_csv("./data/singer.csv", dtype=str)
+    for row in singerDF.itertuples():
+        singer = Singer(singer_name=row[1], gender=row[2] ,language=row[3])
+        db.session.add(singer)
+    db.session.commit()
+
     musicsDF = pd.read_csv("./data/song.csv", dtype=str)
     for row in musicsDF.itertuples():
         music = Music(song_name=row[2], type=row[3], singer_name=row[4])
         db.session.add(music)
     
-    singerDF = pd.read_csv("./data/singer.csv", dtype=str)
-    for row in singerDF.itertuples():
-        singer = Singer(singer_name=row[2], gender=row[3] ,language=row[4])
-        db.session.add(singer)
-
     db.session.commit()
     click.echo('Done.')
 
@@ -50,14 +51,14 @@ class Music(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     song_name = db.Column(db.String(50))
     type = db.Column(db.String(20))
-    singer_name = db.Column(db.String(50))
+    singer_name = db.Column(db.String(50), db.ForeignKey("singer.singer_name"))
 
 
 class Singer(db.Model):
     __tablename__ = 'singer'
         
-    singer_id = db.Column(db.Integer, primary_key=True)
-    singer_name = db.Column(db.String(50))
+    # singer_id = db.Column(db.Integer, primary_key=True)
+    singer_name = db.Column(db.String(50), primary_key=True)
     gender = db.Column(db.String(20))
     language = db.Column(db.String(30))
 
@@ -88,6 +89,7 @@ def index():
 
 @app.route('/singer', methods=['GET', 'POST'])
 def SingerPage():
+    # to do
     # if request.method == 'POST':
     #     song_name = request.form['song_name']
     #     type = request.form['type']
@@ -112,16 +114,16 @@ def edit(music_id):
 
     if request.method == 'POST':
         song_name = request.form['song_name']
-        type = request.form['type']
-        singer_name = request.form['singer_name']
+        # type = request.form['type']
+        # singer_name = request.form['singer_name']
 
         # if not title or not type or len(type) > 4 or len(title) > 60:
         #     flash('Invalid input.')
         #     return redirect(url_for('edit', movie_id=movie_id))
 
         music.song_name = song_name
-        music.type = type   
-        music.singer_name = singer_name
+        # music.type = type   
+        # music.singer_name = singer_name
         db.session.commit()
         flash('Item updated.')
         return redirect(url_for('index'))
@@ -129,13 +131,18 @@ def edit(music_id):
     return render_template('edit.html', music=music)
 
 
-@app.route('/delete/<int:music_id>', methods=['POST'])
+@app.route('/music/delete/<int:music_id>', methods=['POST'])
 def delete(music_id):
     music = Music.query.get_or_404(music_id)
     db.session.delete(music)
     db.session.commit()
     flash('Item deleted.')
     return redirect(url_for('index'))
+
+# @app.route('/ShowSinger/<Asinger>', methods=['GET'])
+# def ShowSinger(Asinger):
+#     ASinger = Singer.query.get_or_404(singer)
+#     return redirect(url_for('ShowSinger'), ASinger=ASinger)
 
 if __name__ == '__main__':
     app.run(debug=True)
